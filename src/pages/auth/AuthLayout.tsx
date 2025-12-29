70b76b721f6633c78089fffb13708dfa212989a3
Metehan Atakan <matakan@bearcat.kartaca.net>
2025-12-29 15:30:27 +0300

    Auth responsive fix

--- src/pages/auth/AuthLayout.tsx
+++ src/pages/auth/AuthLayout.tsx
@@ -5,11 +5,11 @@ import signInImage from '@/assets/sign-in.png'
 export function AuthLayout() {
   return (
     <div className="flex h-screen w-full overflow-hidden">
-      <div className="flex flex-[3] flex-col bg-white overflow-y-auto pl-48">
-        <div className="px-8 pt-8 pb-4 shrink-0">
+      <div className="flex flex-[3] flex-col bg-white overflow-y-auto px-4 sm:px-8 lg:pl-48">
+        <div className="px-4 sm:px-8 lg:px-0 pt-8 pb-4 shrink-0">
           <Logo />
         </div>
-        <div className="flex-1 flex items-center  px-8 py-12">
+        <div className="flex-1 flex items-center justify-center lg:justify-start px-4 sm:px-8 lg:px-0 py-12">
           <div className="w-full max-w-md">
             <Outlet />
           </div>
